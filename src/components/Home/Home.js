b16fb4663facc88a051532e03b9db088aa20c1be
mdNaimulHoque <naimulhoq89@gmail.com>
2020-10-02 01:28:44 +0600

    header navbar design is done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Button, Form, FormControl, Nav, Navbar,  } from 'react-bootstrap';
 import logo from '../../images/Logo.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.css';
 
 const Home = () => {
     return (
-        <Navbar>
-            <Navbar.Brand>
-                
+         <Navbar bg="" expand="lg" className="navbar-container">
+            <Navbar.Brand href="#home">
+                <img 
+                        src={logo} 
+                        width="150" 
+                        height="50"
+                        className="d-inline-block- align-top logo-img" 
+                        alt="Logo" />
             </Navbar.Brand>
-        </Navbar>
+                <FormControl type="text" placeholder="Search" className="form-control" />
+            <Nav className="mr-auto nav-link">
+                <Nav.Link href="#home">News</Nav.Link>
+                <Nav.Link href="#features">Destination</Nav.Link>
+                <Nav.Link href="#pricing">Blog</Nav.Link>
+                <Nav.Link href="#pricing">Contact</Nav.Link> 
+            </Nav>
+            <Button variant="warning" className="navbar-btn">Search</Button>
+        </Navbar> 
     );
 };
 

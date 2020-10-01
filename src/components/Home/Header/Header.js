c861f4ec5bf0304340664880c6bfbd3fe9a0d634
mdNaimulHoque <naimulhoq89@gmail.com>
2020-10-02 01:52:46 +0600

    header is added

---
+++ src/components/Home/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, Form, FormControl, Nav, Navbar,  } from 'react-bootstrap';
+import logo from '../../../images/Logo.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <Navbar bg="" expand="lg" className="navbar-container">
+            <Navbar.Brand href="#home">
+                <img 
+                        src={logo} 
+                        width="150" 
+                        height="50"
+                        className="d-inline-block- align-top logo-img" 
+                        alt="Logo" />
+            </Navbar.Brand>
+                <FormControl type="text" placeholder="Search" className="form-control" />
+            <Nav className="mr-auto nav-link">
+                <Nav.Link href="#home">News</Nav.Link>
+                <Nav.Link href="#features">Destination</Nav.Link>
+                <Nav.Link href="#pricing">Blog</Nav.Link>
+                <Nav.Link href="#pricing">Contact</Nav.Link> 
+            </Nav>
+            <Button variant="warning" className="navbar-btn">Search</Button>
+        </Navbar> 
+    );
+};
+
+export default Header;

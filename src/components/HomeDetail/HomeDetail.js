9582bf3976c1203680094bbb0f055dac221d0b7f
mdNaimulHoque <naimulhoq89@gmail.com>
2020-10-02 04:54:34 +0600

    home page disign is done

--- src/components/HomeDetail/HomeDetail.js
+++ src/components/HomeDetail/HomeDetail.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Card, CardDeck, CardGroup, Col, Container, Row } from 'react-bootstrap';
 import './HomeDetail.css';
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faChevronCircleRight, faChevronCircleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const HomeDetail = () => {
     return (
@@ -15,10 +16,25 @@ const HomeDetail = () => {
                         <Button variant="warning"> Booking  <FontAwesomeIcon icon={faArrowRight} /></Button>
                     </Col>
                     <Col>
-                        <h1>This is num 2</h1>
+                        <Row className="img-row">
+                            <Col className="coxbazar-img size">
+                                <h5 className="bottom-left">COX'S BAZAR</h5>
+                            </Col>
+                            <Col className="sreemongle-img size">
+                                <h5 className="bottom-left">SREEMONGLE</h5>
+                            </Col>
+                            <Col className="sundorbans-img size">
+                                <h5 className="bottom-left">SUNORBANS</h5>
+                            </Col>  
+                        </Row>                      
                     </Col>
                 </Row>
+                
             </Container>
+            <div className="centered">
+                    <FontAwesomeIcon icon={faChevronCircleLeft} />
+                    <FontAwesomeIcon icon={faChevronCircleRight} />
+            </div>
         </div>
     );
 };

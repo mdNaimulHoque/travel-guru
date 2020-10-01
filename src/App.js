829f4ecc9ed4149cbd15e360ff6bad6ed100c221
mdNaimulHoque <naimulhoq89@gmail.com>
2020-10-01 22:17:31 +0600

    now navbar is adding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Now, Start Travel Guru</h1>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import logo from '../../images/Logo.png'
+
+const Home = () => {
+    return (
+        <Navbar>
+            <Navbar.Brand>
+                
+            </Navbar.Brand>
+        </Navbar>
+    );
+};
+
+export default Home;
